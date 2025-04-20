28beead9e73377a7e430204dad4bb14492683be7
Syed Faisal Hassan <faisalhassan77889@gmail.com>
2025-04-21 02:20:42 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/test",(req,res)=>{
+    res.send("Hello world");
+})
+
+
+
+app.listen("5000",()=>{
+    console.log("server is listening on port 5000...")
+})
